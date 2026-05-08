ffffdc752352cf92d207fc6241befea6d8fdd4d0
Siddhant Srivastava <siddhantsrivastava20feb@gmail.com>
2026-05-09 04:19:46 +0530

    handleFindBlood

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -2,10 +2,38 @@ import React from 'react'
 import { useNavigate } from 'react-router-dom'
 import Navbar from '../../components/Navbar/Navbar'
 import './Home.css'
+
 const Home = () => {
+
   return (
-    <div>
-        
+    <div className="home">
+      <Navbar />
+      <section className="hero">
+        <img src="/hero.png" alt="Donify Hero" className="hero-image" />
+        <div className="hero-overlay">
+          <div className="hero-content">
+            <h1>
+              Blood when
+              <br />
+              it matters.
+              <br />
+              <span>Found in seconds.</span>
+            </h1>
+            <p className="hero-desc">
+              Connecting donors and those in need.
+              <br />
+              Because every drop can save a life.
+            </p>
+            <div className="hero-buttons">
+              <button className="find-btn" 
+              onClick={handleFindBlood}
+              >
+                Find Blood Now →
+              </button>
+            </div>
+          </div>
+        </div>
+      </section>
     </div>
   )
 }
